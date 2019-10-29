b57714b755e1a17163904a2d6deb9e3ae484a93f
LuckDev <lucaspdpp@gmail.com>
2019-10-28 21:13:39 -0300

    Upload exercise

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,80 @@
+import styled, { keyframes, css } from 'styled-components';
+
+export const Form = styled.form`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+
+  input {
+    flex: 1;
+    border: 1px solid #eee;
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 16px;
+    transition-duration: 0.2s;
+  }
+
+  input:focus {
+    border: 1px solid #7159c1;
+    transition-duration: 0.2s;
+  }
+`;
+
+const rotate = keyframes`
+  from{
+    transform: rotate(0deg);
+  }
+
+  to{
+    transform: rotate(360deg);
+  }
+`;
+
+export const SubmitButton = styled.button.attrs(props => ({
+  type: 'submit',
+  disabled: props.load,
+}))`
+  background: #7159c1;
+  border: 0;
+  padding: 0 15px;
+  margin-left: 10px;
+  border-radius: 4px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+  ${props =>
+    props.load &&
+    css`
+      svg {
+        animation: ${rotate} 2s linear infinite;
+      }
+    `};
+`;
+
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+
+  li {
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    & + li {
+      border-top: 1px solid #eee;
+    }
+
+    a {
+      color: #7159c1;
+      text-decoration: none;
+    }
+  }
+`;
